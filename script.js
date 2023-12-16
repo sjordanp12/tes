133a0374fca2c60f7e2eb78df810bf73c1439cb3
Septian Jordan Purba <142944749+sjordanp12@users.noreply.github.com>
2023-12-16 11:54:21 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// // Saat pengguna scroll ke bawah 20px dari atas dokumen, geser ke bawah bar navigasi
+// window.onscroll = function () {
+//   scrollFunction();
+// };
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     document.getElementById('navbar').style.top = '0';
+//   } else {
+//     document.getElementById('navbar').style.top = '0px';
+//   }
+// }
